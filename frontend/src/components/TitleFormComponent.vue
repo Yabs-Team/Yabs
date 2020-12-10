b80980894d85b59ce7f225c15c8f716ad592253e
adr29truck <adrianalmetunsmeds@gmail.com>
2020-12-10 11:21:23 +0100

    Removed unused import

--- frontend/src/components/TitleFormComponent.vue
+++ frontend/src/components/TitleFormComponent.vue
@@ -75,7 +75,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 import TitlesModule from '../store/modules/TitlesModule';
 import { Title, TitleForm } from '../types';
 
