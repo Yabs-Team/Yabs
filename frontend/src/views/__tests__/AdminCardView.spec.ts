6f42771aec0ad0abcc7106fbfeff2471cf76facd
Jacob Sandström <jojacocobob@gmail.com>
2020-12-11 14:38:33 +0100

    Test if CardFormComponent is present in view

---
+++ frontend/src/views/__tests__/AdminCardView.spec.ts
@@ -0,0 +1,15 @@
+
+
+import { shallowFactory } from '@/helpers/testFactoryHelpers';
+import { Wrapper } from '@vue/test-utils';
+import AdminCardsView from '../AdminCardsView.vue';
+
+describe('AdminCardsView.vue', () => {
+  
+  it('CardFormComponent exists in view', () => {
+    const wrapper: Wrapper<AdminCardsView> = shallowFactory(AdminCardsView);
+    expect(wrapper.findComponent({name: 'CardFormComponent'}).exists()).toBe(true);
+
+  });
+
+});
