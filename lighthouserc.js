e1e31081bc84d87dfc1ca838c1db20dffa729a5b
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-12-14 14:59:08 +0100

    Use port for test env

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:8080/', 'http://localhost:8080/admin', 'http://localhost:8080/find'],
+      url: ['http://localhost:3000/', 'http://localhost:3000/admin', 'http://localhost:3000/find'],
     },
     upload: {
       target: 'temporary-public-storage',
