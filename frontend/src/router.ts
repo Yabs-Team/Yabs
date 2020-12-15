9cc63b006a5702e7b3d6cbc306962ab73dbb949e
te4-alexander-kjellberg <alexander.kjellberg@elev.ga.ntig.se>
2020-12-15 10:16:48 +0100

    potentially fixed issues with router.ts

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,49 +1,53 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter, { RouterOptions } from 'vue-router';
+import { RouteConfig } from 'vue-router/types/router';
 import * as views from '@/views';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    name: 'start',
+    component: views.Start,
+  },
+  {
+    path: '/users/:id',
+    name: 'profile',
+    component: views.Profile,
+  },
+  {
+    path: '/books/:id',
+    name: 'edit book',
+    component: views.EditBook,
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: views.AdminView,
+    children: [
+      {path: '', component: views.AdminDashboardView},
+      {path: 'loans', component: views.AdminLoansView},
+      {path: 'titles', component: views.AdminTitlesView},
+      {path: 'books', component: views.AdminBooksView},
+      {path: 'cards', component: views.AdminCardsView},
+      {path: 'users', component: views.AdminUsersView},
+    ]
+  },
+  {
+    path: '/find',
+    name: 'find',
+    component: views.Find,
+  },
+  {
+    path: '/library',
+    name: 'library',
+    component: views.Library,
+  }
+];
+const options: RouterOptions = {
+  base: process.env.BASE_URL,
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'start',
-      component: views.Start,
-    },
-    {
-      path: '/users/:id',
-      name: 'profile',
-      component: views.Profile,
-    },
-    {
-      path: '/books/:id',
-      name: 'edit book',
-      component: views.EditBook,
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: views.AdminView,
-      children: [
-        {path: '', component: views.AdminDashboardView},
-        {path: 'loans', component: views.AdminLoansView},
-        {path: 'titles', component: views.AdminTitlesView},
-        {path: 'books', component: views.AdminBooksView},
-        {path: 'cards', component: views.AdminCardsView},
-        {path: 'users', component: views.AdminUsersView},
-      ]
-    },
-    {
-      path: '/find',
-      name: 'find',
-      component: views.Find,
-    },
-    {
-      path: '/library',
-      name: 'library',
-      component: views.Library,
-    }
-  ],
-});
+  routes,
+};
+export default new VueRouter(options);
