f3812a3bfc3b10cb7d29d429db515186cff03cff
David Jensen <david.jensen@elev.ga.ntig.se>
2020-12-16 18:39:30 +0100

    Changed successful 'DELETE'-request HTTP code to 204

--- backend/app/controllers/api/v1/loans_controller.rb
+++ backend/app/controllers/api/v1/loans_controller.rb
@@ -49,7 +49,7 @@ class Api::V1::LoansController < ApplicationController
   def destroy
     authorize @loan
     @loan.destroy
-    render json: @loan.id
+    render json: @loan.id, status: :no_content
   end
 
   # this is the private set loan method to find the current loan based on the given params
