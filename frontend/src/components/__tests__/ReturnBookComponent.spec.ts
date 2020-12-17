9fc2a4890724750dfb66426d82e15a20ac80137b
Jonathan Ronsten <jonathan.ronsten@elev.ga.ntig.se>
2020-12-17 14:10:10 +0100

    Linting fixses

--- frontend/src/components/__tests__/ReturnBookComponent.spec.ts
+++ frontend/src/components/__tests__/ReturnBookComponent.spec.ts
@@ -5,27 +5,27 @@ import ReturnLoanComponent from '@/components/ReturnLoanComponent.vue';
 import BooksModule from '@/store/modules/BooksModule';
 
 jest.mock('@/store/modules/BooksModule', () => {
-    return {
-      create: jest.fn().mockImplementationOnce((_arg) => {
-        return Promise.resolve();
-      })
-    };
+  return {
+    create: jest.fn().mockImplementationOnce((_arg) => {
+      return Promise.resolve();
+    })
+  };
 });
 
 describe('ReturnLoanComponent.vue', () => {
-    it('calls update book with correct input', () => {
-        setTimeout(() => {
-            const wrapper: Wrapper<ReturnLoanComponent> = shallowFactory(ReturnLoanComponent);
+  it('calls update book with correct input', () => {
+    setTimeout(() => {
+      const wrapper: Wrapper<ReturnLoanComponent> = shallowFactory(ReturnLoanComponent);
     
-            wrapper.find('[data-jest=\'book_barcode_return\']').setValue('5002');
-            wrapper.find('[data-jest=\'book_condition\']').setValue('Broken');
-            wrapper.find('[data-jest=\'ReturnBook\']').trigger('submit');
+      wrapper.find('[data-jest=\'book_barcode_return\']').setValue('5002');
+      wrapper.find('[data-jest=\'book_condition\']').setValue('Broken');
+      wrapper.find('[data-jest=\'ReturnBook\']').trigger('submit');
     
-            expect(BooksModule.update).toHaveBeenCalledWith({
-                barcode: '5002', 
-                title_id: 1, //eslint-disable-line camelcase
-                condition: 'Broken' 
-            });
-        }, 1000);
-    });
+      expect(BooksModule.update).toHaveBeenCalledWith({
+        barcode: '5002', 
+        title_id: 1, //eslint-disable-line camelcase
+        condition: 'Broken' 
+      });
+    }, 1000);
+  });
 });
