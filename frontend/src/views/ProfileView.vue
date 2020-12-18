b293380644665da81418df0a98da2677e299de26
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2020-12-18 09:43:24 +0100

    Change images

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -10,13 +10,66 @@
         <v-card>
           <v-card-title>{{ getUser().name }}</v-card-title>
           <v-card-subtitle>{{ roleToText(getUser().role) }} - {{ getUser().klass }}</v-card-subtitle>
-          <img
+          <v-img
             v-if="getUser().photo_path"
+            class="ma-a image pb-10"
+            contain
             :src="`http://localhost:3000/${getUser().photo_path}`"
           >
+            <template v-slot:placeholder>
+              <v-row
+                class="fill-height ma-0"
+                align="center"
+                justify="center"
+              >
+                <v-progress-circular
+                  indeterminate
+                  color="black lighten-5"
+                />
+              </v-row>
+            </template>
+          </v-img>
         </v-card>
       </v-col>
     </v-row>
+
+    <v-card
+      v-if="RoleChecker.isAdmin()"
+      class="d-flex align-start justify-center pa-4 pt-7 pb-1"
+    >
+      <v-file-input
+        v-model="image"
+        class="pa-0"
+        color="deep-purple accent-4"
+        label="File input"
+        multiple
+        placeholder="Update image"
+        prepend-icon="mdi-paperclip"
+        outlined
+      />
+      <v-btn
+        class="mt-1 ml-10"
+        color="primary"
+        large
+        @click="savePicture"
+      >
+        Uppdatera
+      </v-btn>
+    </v-card>
+
+    <v-snackbar v-model="snackbar">
+      {{ snackbarText }}
+      <template>
+        <v-btn
+          color="primary"
+          text
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+
     <v-row>
       <v-col col="12">
         <v-card>
@@ -54,6 +107,9 @@ export default class Profile extends Vue {
   private usersModule: UserModuleType = UsersModule;
   private RoleChecker: RoleChecker = RoleChecker;
   private roleToText: Function = roleToText;
+  public image: File[] = [];
+  public snackbar: Boolean = false;
+  public snackbarText: String = '';
 
   private created(): void {
     LoansModule.fetchAll();
@@ -62,5 +118,40 @@ export default class Profile extends Vue {
   private getUser():User {
     return this.usersModule.all[+this.$route.params.id];
   }
+
+  public savePicture(): void {
+    let errorText = '';
+
+    if(this.image.length == 0){
+      errorText = 'No files selected. Please select an image!';
+    }else if(this.image.length > 1){
+      errorText = 'Too many files selected! Please select only one!';
+    }
+
+    if(errorText != ''){
+      this.snackbarText = errorText;
+      this.snackbar = true;
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('uid', this.$route.params.id);
+    formData.append('image', this.image[0] as Blob);
+    UsersModule.update(formData).then((response: User) => {
+      this.snackbarText = 'Users image has been updated!';
+    }).catch((error: object) => {
+      this.snackbarText = error.toString();
+    });
+    
+    this.snackbar = true;
+  }
 }
 </script>
+
+<style lang="scss">
+
+  .image{
+    width: 50%;
+    margin: auto;
+  }
+</style>
