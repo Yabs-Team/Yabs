112ac20fbb63fde4a8c770325b3713f85671725a
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2020-12-18 15:05:02 +0100

    Removed unnecessary comments

--- frontend/cypress/integration/AdminCards.js
+++ frontend/cypress/integration/AdminCards.js
@@ -1,10 +1,4 @@
-// const { expect } = require("chai")
-// const { it } = require("mocha")
 import baseVisit from "../helper"
-
-// const { it } = require("mocha")
-
-// const { default: baseVisit } = require("../helper")
 
 describe('Testing for /admin/cards', () => {
 
@@ -22,13 +16,10 @@ describe('Testing for /admin/cards', () => {
     })
 
     it('Tries to select a user for the card', () => {
-        cy.get('[data-cy=user-selector]').click().first().click();
-        // cy.get('[data-cy=user-selector]').type('a').type('{enter}');
-        
+        cy.get('[data-cy=user-selector]').click().first().click();        
     })
 
     it('Tries to download card', () => {
         cy.get('[data-cy=download-btn]').click();
-        //Is there a way for cypress to check if file is being downloaded?
     })
 })
