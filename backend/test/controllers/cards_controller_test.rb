33405a5ea034f77aabd08de7442a8623df96cf7b
Jacob Sandström <jojacocobob@gmail.com>
2020-12-18 15:47:50 +0100

    attempt to fix CardControllerTest

--- backend/test/controllers/cards_controller_test.rb
+++ backend/test/controllers/cards_controller_test.rb
@@ -6,41 +6,41 @@ class CardsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get cards_url
+    get api_v1_cards_url
     assert_response :success
   end
 
   test "should get new" do
-    get new_card_url
+    get api_v1_new_card_url
     assert_response :success
   end
 
   test "should create card" do
     assert_difference('Card.count') do
-      post cards_url, params: { card: { expiration_data: @card.expiration_data, status: @card.status, uid: @card.uid, user_uid: @card.user_uid } }
+      post api_v1_cards_url, params: { card: { expiration_data: @card.expiration_data, status: @card.status, uid: @card.uid, user_uid: @card.user_uid } }
     end
 
-    assert_redirected_to card_url(Card.last)
+    assert_redirected_to api_v1_card_url(Card.last)
   end
 
   test "should show card" do
-    get card_url(@card)
+    get api_v1_card_url(@card)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_card_url(@card)
+    get api_v1_edit_card_url(@card)
     assert_response :success
   end
 
   test "should update card" do
-    patch card_url(@card), params: { card: { expiration_data: @card.expiration_data, status: @card.status, uid: @card.uid, user_uid: @card.user_uid } }
-    assert_redirected_to card_url(@card)
+    patch api_v1_card_url(@card), params: { card: { expiration_data: @card.expiration_data, status: @card.status, uid: @card.uid, user_uid: @card.user_uid } }
+    assert_redirected_to api_v1_card_url(@card)
   end
 
   test "should destroy card" do
     assert_difference('Card.count', -1) do
-      delete card_url(@card)
+      delete api_v1_card_url(@card)
     end
 
     assert_redirected_to cards_url
