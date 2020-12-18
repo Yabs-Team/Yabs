86662e533a39096d4b527f68eb7a6c465388a601
David Jensen <david.jensen@elev.ga.ntig.se>
2020-12-18 14:00:52 +0100

    Added unit tests for AdminTitlesView

---
+++ frontend/src/views/__tests__/AdminTitlesView.spec.ts
@@ -0,0 +1,21 @@
+import { shallowFactory } from '@/helpers/testFactoryHelpers';
+import { Wrapper } from '@vue/test-utils';
+import AdminTitlesView from '../AdminTitlesView.vue';
+import Vue from 'vue';
+
+describe('AdminTitlesView.vue', () => {
+  
+  it('Is not empty', () => {
+    const wrapper: Wrapper<AdminTitlesView> = shallowFactory(AdminTitlesView);
+    expect(wrapper.find('div').exists()).toBe(true);
+  });
+  
+  it('Components exist in view', () => {
+    const injectRouterStubs = {stubs: ['router-link']};
+    const wrapper: Wrapper<Vue> = shallowFactory(AdminTitlesView, injectRouterStubs);
+    const componentNames = ['TitleFormComponent', 'TitleListComponent'];
+    componentNames.forEach(componentName => {
+      expect(wrapper.find(`${componentName}-stub`).name()).toEqual(componentName);
+    });
+  });
+});
