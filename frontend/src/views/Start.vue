2f69e8e5f71bfd239d790630a823d870c8555980
David Jensen <david.jensen@elev.ga.ntig.se>
2020-12-18 12:09:15 +0100

    Implemented tests for views/start

--- frontend/src/views/Start.vue
+++ frontend/src/views/Start.vue
@@ -87,16 +87,16 @@
   LoanListComponent in order to render the loans that soon are to expire
 -->
 <script lang="ts">
-import { SetupContext } from '@vue/composition-api';
+import { defineComponent, SetupContext } from '@vue/composition-api';
 import LoanListComponent from '@/components/LoanListComponent.vue';
 import UsersModule from '../store/modules/UsersModule';
 import TitlesModule from '../store/modules/TitlesModule';
 import LoansModule from '../store/modules/LoansModule';
 import BooksModule from '../store/modules/BooksModule';
-import { VuexModule } from 'vuex-module-decorators';
+import { UserModuleType } from '../types';
 
-export default ({
-  name: '',
+export default defineComponent({
+  name: 'StartView',
   components: {
     LoanListComponent
   },
@@ -104,7 +104,7 @@ export default ({
     const size: string = 'lg';
     const primary: string = 'primary';
     const loading: boolean = true;
-    const usersModule: VuexModule = UsersModule;
+    const usersModule: UserModuleType = UsersModule;
 
     // The users module is imported and used in order to get information about the current user
     // but also so that the possibility for a user to monitor its soon expiring loans.
@@ -128,6 +128,8 @@ export default ({
     };
   }
 });
+
+
 </script>
 
 
