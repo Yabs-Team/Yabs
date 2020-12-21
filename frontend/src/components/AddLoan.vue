c85b7420dd85b4a4ba38b9bb8f19d8a5828aa898
Henrik Stahl <henrik.stahl@elev.ga.ntig.se>
2020-12-21 13:06:00 +0100

    refactored addLoan

--- frontend/src/components/AddLoan.vue
+++ frontend/src/components/AddLoan.vue
@@ -119,9 +119,11 @@ export default defineComponent({
     function onShown(): void {
       focusRef(input);
     }
+
     function onHidden() : void {
       focusRef(button);
     }
+    
     function focusRef(ref : any) : void { //eslint-disable-line @typescript-eslint/no-explicit-any
       root.$nextTick(() => {
         root.$nextTick(() => {
@@ -129,11 +131,13 @@ export default defineComponent({
         });
       });
     }
+    
     watch(input,(value, prevValue) => {
       if (value) {
         inputState.value = true;
       }
     });
+    
     return{
       input,
       inputState,
@@ -149,71 +153,3 @@ export default defineComponent({
   }
 });
 </script>
-
-
-
-//   }
-
-//   public onOk(): void {
-//     if (!this.input) {
-//       this.inputState = false;
-//     }
-//     if (this.input) {
-//       this.onCancel();
-//       /* "Return" our popover "form" results */
-//       this.inputReturn = this.input;
-//       this.form.book_id = parseInt(this.input); //eslint-disable-line camelcase
-//       this.form.loaned_by_id = UsersModule.currentUserID; //eslint-disable-line camelcase
-//       console.log(this.form);
-//       /* This Checks if the form contains the necessary data  */
-//       if (!!this.form.lent_by_id && !!this.form.loaned_by_id && !!this.form.book_id) {
-//         LoansModule.create(this.form)
-//           .then((payload: Loan) => this.$emit('loan-added', payload))
-//           .catch((failure: boolean) => console.log(failure));
-//         this.input = '';
-//       }
-
-    
-//     }
-//   }
-
-//   public onShow(): void {
-//     /* This is called just before the popover is shown */
-//     /* Reset our popover "form" variables */
-//     this.input = '';
-//     this.inputState = null;
-//     this.inputReturn = '';
-//   }
-
-//   public onShown(): void {
-//     /* Called just after the popover has been shown */
-//     /* Transfer focus to the first input */
-//     this.focusRef(this.$refs.input);
-//   }
-
-//   public onHidden(): void {
-//     /* Called just after the popover has finished hiding */
-//     /* Bring focus back to the button */
-//     this.focusRef(this.$refs.button);
-//   }
-
-//   public focusRef(ref: any): void { //eslint-disable-line @typescript-eslint/no-explicit-any
-//     /* Some references may be a component, functional component, or plain element */
-//     /* This handles that check before focusing, assuming a focus() method exists */
-//     /* We do this in a double nextTick to ensure components have updated & popover positioned first */
-//     this.$nextTick(() => {
-//       this.$nextTick(() => {
-//         (ref.$el || ref).focus();
-//       });
-//     });
-//   }
-
-//   @Watch('input')
-//   public onInputChange(val: string, oldVal: string): void {
-//     if (val) {
-//       this.inputState = true;
-//     }
-//   }
-// }
-
-
