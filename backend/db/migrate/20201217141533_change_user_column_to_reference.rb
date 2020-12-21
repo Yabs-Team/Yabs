403589f15dd8b5d10727a5a4b7acfb9df228f053
Jacob-sandström <43438069+Te4-Jacob-sandstrom@users.noreply.github.com>
2020-12-21 11:04:06 +0100

    Update 20201217141533_change_user_column_to_reference.rb

--- backend/db/migrate/20201217141533_change_user_column_to_reference.rb
+++ backend/db/migrate/20201217141533_change_user_column_to_reference.rb
@@ -1,7 +1,6 @@
 class ChangeUserColumnToReference < ActiveRecord::Migration[6.0]
   def up
     remove_column :cards, :user_uid
-    # add_reference :cards, :user, foreign_key: true
     add_reference :cards, :user, type: :string, :class_name => 'User', :primary_key => 'uid'
 
   end
