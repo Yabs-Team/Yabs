18195143e3bb07e6431f8b60139146eac70e21dc
Jacob Sandström <jojacocobob@gmail.com>
2020-12-21 10:22:56 +0100

    .

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_143259) do
+ActiveRecord::Schema.define(version: 2020_12_21_091748) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_143259) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "user_id", null: false
+    t.integer "user_id", null: false
     t.index ["uid"], name: "index_cards_on_uid", unique: true
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
