0f90230fa86aac56fd7e67d78146249ca8ce7a5c
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2021-01-06 19:43:36 +0100

    Added a baseModal

---
+++ frontend/src/components/BaseModal.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="showModal"
+      persistent
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          {{ header }}
+        </v-card-title>
+        <v-card-text>
+          {{ body }}
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            v-for="x in actions"
+            :key="x.text"
+            color="green darken-1"
+            text
+            @click="x.action"
+          >
+            {{ x.text }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script lang="ts">
+import {ref, defineComponent } from '@vue/composition-api';
+
+
+interface BaseModalActionProp {
+  text: string;
+  action: Function;
+}
+
+interface BaseModalProps {
+  showModal: Boolean;
+  actions: BaseModalActionProp[]
+}
+
+function defaultPropFactory(): BaseModalActionProp[]  {
+  return {default: [{ text: 'Nooo', action: (): void => { console.log('noo'); }}, { text: 'Yeah', action: (): void => { console.log('Yeah'); }}]};
+}
+
+export default defineComponent({
+  name: 'BaseModal',
+
+  props: {
+    header: {default: 'This header', type: String},
+    body: {default: 'This body', type: String},
+    showModal: {default: false, type: Boolean},
+    actions: defaultPropFactory(),
+  },
+});
+</script>
