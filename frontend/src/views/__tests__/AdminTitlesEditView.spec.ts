98f9774d2211204befe3e6b0cde97adfa9646776
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2021-01-11 12:26:34 +0100

    Linting AdminTitlesEditView

--- frontend/src/views/__tests__/AdminTitlesEditView.spec.ts
+++ frontend/src/views/__tests__/AdminTitlesEditView.spec.ts
@@ -1,26 +1,26 @@
-import AdminTitlesEditView from "../AdminTitlesEditView.vue";
-import vue from "vue";
+import AdminTitlesEditView from '../AdminTitlesEditView.vue';
+import vue from 'vue';
 
-import { Wrapper } from "@vue/test-utils";
-import { shallowFactory } from "@/helpers/testFactoryHelpers";
+import { Wrapper } from '@vue/test-utils';
+import { shallowFactory } from '@/helpers/testFactoryHelpers';
 
-jest.mock("@/store/modules/TitlesModule");
+jest.mock('@/store/modules/TitlesModule');
 
 const options = {
   mocks: {
     $route: {
       params: {
-        id: "0763645680"
+        id: '0763645680'
       }
     }
   },
   sync: false
 };
 
-describe("AdminTitlesEditView.vue", () => {
-  it("Displays the correct data in the form", async () => {
+describe('AdminTitlesEditView.vue', () => {
+  it('Displays the correct data in the form', async () => {
     const wrapper: Wrapper<vue> = shallowFactory(AdminTitlesEditView, options);
     expect(wrapper.html()).toMatchSnapshot();
-    expect(wrapper.text()).not.toMatch("Clean Code");
+    expect(wrapper.text()).not.toMatch('Clean Code');
   });
 });
