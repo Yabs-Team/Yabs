4fcca5cb18156c731bf9d371f3a7102f1c975ac5
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2021-01-11 10:03:13 +0100

    Added unit tests for  AdminTitlesEdit view

---
+++ frontend/src/store/modules/__mocks__/TitlesModule.ts
@@ -0,0 +1,39 @@
+import { Title } from "@/types";
+
+const title: Title = {
+  "id": 1,
+  "name": "Alice in Wonderland",
+  "isbn": "0763645680",
+  "cost": 700,
+  "title_type": "Skönlitteratur",
+  "created_at": "2020-12-21T15:23:15.615Z",
+  "updated_at": "2021-01-04T15:54:04.695Z",
+};
+const title1: Title = {
+  "id": 3,
+  "name": "Clean Code",
+  "isbn": "9780132350884",
+  "cost": 300,
+  "title_type": "Kurslitteratur",
+  "created_at": "2020-12-21T15:23:15.652Z",
+  "updated_at": "2020-12-21T15:23:15.652Z",
+};
+
+module.exports = {
+  allAsArray: [title, title1],
+  fetchAll: jest.fn().mockImplementationOnce(() => {
+    return Promise.resolve();
+  }),
+  create: jest.fn().mockImplementationOnce(_arg => {
+    return Promise.resolve();
+  }),
+  update: jest.fn(),
+  fetchSingle: (_: any) => {
+    return title;
+  },
+  fetchSingleByISBN: jest.fn().mockImplementation(() => {
+    return new Promise((resolve, _) => {
+      resolve(title);
+    })
+  }),
+};
