f60010094b95699a4d656ad3d27b3f6d0f351222
Te4-Dennis-Christensen <dennis.christensen@elev.ga.ntig.se>
2021-01-11 14:47:33 +0100

    Refactor done

--- frontend/src/components/GoogleLogin.vue
+++ frontend/src/components/GoogleLogin.vue
@@ -4,90 +4,94 @@
     text
     color="grey"
   >
-    <div id="signin2" />
+    <div 
+      id="signin2"
+      ref="signin2"
+    />
   </v-btn>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { defineComponent, Ref, ref, SetupContext } from '@vue/composition-api';
 import UsersModule from '../store/modules/UsersModule';
 import { VuexModule } from 'vuex-module-decorators';
 
-@Component
-export default class GoogleLogin extends Vue {
-  public usersModule: VuexModule = UsersModule;
+export default defineComponent({
+  name: 'GoogleLogin',
+  setup(_: Object, {root}: SetupContext){
+    const signin2: Ref<HTMLDivElement | null> = ref(null);
+    // renderLoginButton is used to set the information about the button without having to 
+    // include any css, which is forbidden in this project, based on the class signin2 
 
-  // mounted is a getter to reveive the information about the renderloginbutton
-
-  public mounted(): void {
-    this.renderLoginButton();
-  }
-
-  // renderLoginButton is used to set the information about the button without having to 
-  // include any css, which is forbidden in this project, based on the class signin2 
-
-  public renderLoginButton(): void {
-
-    // @ts-ignore: gapi
-    gapi.signin2.render('signin2', {
-      scope: 'profile email',
-      width: 200,
-      height: 50,
-      longtitle: false,
-      theme: 'dark',
-      onsuccess: this.onSignIn,
-      onfailure: this.onFailure,
-    });
-  }
-
-  // Getter to get the updated version of the instance renderLoginButton
-
-  public updated(): void {
-    this.renderLoginButton();
-  }
-
-  // onSignIn takes a user from the google Api and sets the users profile with the information
-  // fetched from the google api but also takes the token that is generated and setting it
-  // to the id token so that the authentication is made possbile
-
-  public onSignIn(googleUser: any): void { //eslint-disable-line @typescript-eslint/no-explicit-any
-    console.log(typeof googleUser);
-    const profile = googleUser.getBasicProfile();
-    const idToken: string = googleUser.getAuthResponse().id_token;
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    UsersModule.signIn(idToken)
-      .then((response: object) => {
-        console.log(response);
-      })
-      .catch((error: object) => {
-        this.signOut();
+    function renderLoginButton(): void {
+      // @ts-ignore: gapi
+      gapi.signin2.render('signin2', {
+        scope: 'profile email',
+        width: 200,
+        height: 50,
+        longtitle: false,
+        theme: 'dark',
+        onsuccess: onSignIn,
+        onfailure: onFailure,
       });
-  }
+    }
 
-  // onfailure is a standard method used to render the error message when a process has failed
+    // Getter to get the updated version of the instance renderLoginButton
 
-  public onFailure(error: object): void {
+    function updated(): void {
+      renderLoginButton();
+    }
 
-    // TODO: show this in a notification
+    // onSignIn takes a user from the google Api and sets the users profile with the information
+    // fetched from the google api but also takes the token that is generated and setting it
+    // to the id token so that the authentication is made possbile
 
-    console.error(error);
-  }
+    function onSignIn(googleUser: any): void { //eslint-disable-line @typescript-eslint/no-explicit-any
+      console.log(typeof googleUser);
+      const profile = googleUser.getBasicProfile();
+      const idToken: string = googleUser.getAuthResponse().id_token;
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      UsersModule.signIn(idToken)
+        .then((response: object) => {
+          console.log(response);
+        })
+        .catch((error: object) => {
+          signOut();
+        });
+    }
 
-  // signOut takes the the authentication instance and then log out the user that matches
-  // that description
+    // onfailure is a standard method used to render the error message when a process has failed
 
-  public signOut(): void {
-    // @ts-ignore: gapi
-    const auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(() => {
-      UsersModule.signOut();
+    function onFailure(error: object): void {
+      // TODO: show this in a notification
+      console.error(error);
+    }
+
+    // signOut takes the the authentication instance and then log out the user that matches
+    // that description
+
+    function signOut(): void {
+      // @ts-ignore: gapi
+      const auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(() => {
+        UsersModule.signOut();
+      });
+    };
+    
+    // mounted is a getter to reveive the information about the renderloginbutton
+
+    root.$nextTick(() => {
+      renderLoginButton();
     });
-  }
 
-}
+    return{
+      renderLoginButton, onSignIn, onFailure, signin2
+    };
+  },
+});
 </script>
 
 <style lang="sass" scoped>
