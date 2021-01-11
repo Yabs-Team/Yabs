56086d74ec11c733f30c160a0849cb43bc89fd04
Jacob Sandström <jojacocobob@gmail.com>
2021-01-11 10:37:45 +0100

    updated schema version

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_115452) do
+ActiveRecord::Schema.define(version: 2020_12_21_125752) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.text "name", null: false
