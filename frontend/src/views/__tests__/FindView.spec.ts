905fb0ad9e424cc69f63728f9c6d57467b4393c6
David Jensen <david.jensen@elev.ga.ntig.se>
2021-01-14 10:30:30 +0100

    Implemented feedback

--- frontend/src/views/__tests__/FindView.spec.ts
+++ frontend/src/views/__tests__/FindView.spec.ts
@@ -1,18 +1,25 @@
 import { shallowFactory } from '@/helpers/testFactoryHelpers';
 import { Wrapper } from '@vue/test-utils';
-import FindView from '@/views/FindView.vue';
+import FindView from '../FindView.vue';
 import Vue from 'vue';
 
-describe('Start.vue', () => {
+describe('FindView.vue', () => {
   it('is not empty', () => {
     const wrapper: Wrapper<Vue> = shallowFactory(FindView);
     expect(wrapper.html()).not.toBe('');
   });
-  it('matches the snapshot', () => {
+  it('matches the snapshot', async () => {
     const wrapper: Wrapper<Vue> = shallowFactory(FindView);
+    // sets properties 'users', 'loans' and 'books' to true to allow their respective components to render
+    await wrapper.setData({
+      users: true,
+      loans: true,
+      books: true
+    });
+    // checks if the components rendered as stubs by comparing them to snapshot
     expect(wrapper.html()).toMatchSnapshot();
   });
-  it('router links exists in view', () => {
+  it('requisite v-chips exist with correct content', () => {
     const wrapper: Wrapper<Vue> = shallowFactory(FindView);
     const chipStubs = wrapper.findAll('v-chip-stub');
     for (let i:number = 0; i < chipStubs.length; i++) {
