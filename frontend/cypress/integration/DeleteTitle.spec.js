aac02903b7d4edd8575ce9c38045b70914c38c7e
Jonathan Ronsten <jonathan.ronsten@elev.ga.ntig.se>
2021-01-14 15:03:40 +0100

    Fixed github CI fails

--- frontend/cypress/integration/DeleteTitle.spec.js
+++ frontend/cypress/integration/DeleteTitle.spec.js
@@ -1,23 +1,24 @@
-import baseVisit from '../helper';
+const baseURL = 'localhost'
+function baseVisit(url = '/') {
+    if (Cypress.env('env') == 'test') { cy.visit(`${baseURL}:3000${url}`)}
+    else { cy.visit(`${baseURL}:8080${url}`); } 
+}
 
 context('Going to the first page', () => {
-    beforeEach(() => {
-        baseVisit();
-    });
 
     it('Pressing delete button with active loan, warning should pop up', () => {
-        cy.visit('localhost:8080/admin/titles/0763645680');
+        baseVisit('/admin/titles/0763645680');
         cy.wait(1000);
         cy.get('[data-cy=deleteTitle]').click({force:true});
         cy.wait(1000);
         cy.get('[data-cy=modalBody]').contains('Alex Henryz - 5000');
     });
     it('Pressing delete button without active loan, should delete title', () => {
-        cy.visit('localhost:8080/admin/titles/9781999985462')
-        cy.wait(1000);
+        baseVisit('/admin/titles/9781999985462')
+        cy.wait(2000);
         cy.get('[data-cy=deleteTitle]').click({force:true});
-        cy.wait(1000);
-        cy.visit('localhost:8080/admin/titles/9781999985462')
+        cy.wait(2000);
+        baseVisit('/admin/titles/9781999985462')
         cy.wait(1000);
         cy.get('[data-cy=name]').should('not.contain', 'Tomtens jul')
     });
--- frontend/src/store/modules/__mocks__/TitlesModule.ts
+++ frontend/src/store/modules/__mocks__/TitlesModule.ts
@@ -21,33 +21,33 @@ const title1: Title = {
 };
 
 const user: User = {
-  'uid': 123,
-  'name': 'Example',
-  'email': 'example@example.example',
-  'klass': 'unknown',
+  'uid': 123523,
+  'name': 'abc',
+  'email': 'abc@abc.abc',
+  'klass': 'not known',
   'role': 1,
-  'google_token': '11111111111111111',
-  'photo_path': 'abc.web',
-  'created_at': '2020-12-21T15:22:58.998Z',
-  'updated_at': '2020-12-21T15:22:58.998Z'
+  'google_token': '222222222222',
+  'photo_path': 'ab.web',
+  'created_at': '2021-01-14T15:22:58.998Z',
+  'updated_at': '2021-01-14T15:22:58.998Z'
 };
 
 const book: Book = {
-  barcode: '5002',
+  barcode: '5004',
   note: 'None',
-  condition: 'Broken',
-  created_at: '2020-12-21T15:23:15.866Z', // eslint-disable-line camelcase
+  condition: 'Ok',
+  created_at: '2021-01-14T15:23:15.866Z', // eslint-disable-line camelcase
   title: {
-    cost: 700,
-    created_at: '2020-12-21T15:23:15.615Z',  // eslint-disable-line camelcase
-    id: 1,
-    isbn: '0763645680',
-    name: 'Alice in Wonderland',
+    cost: 70,
+    created_at: '2021-01-14T15:23:15.615Z',  // eslint-disable-line camelcase
+    id: 2,
+    isbn: '123412341',
+    name: 'Test',
     title_type: 'Skönlitteratur',  // eslint-disable-line camelcase
     updated_at: '2021-01-04T15:54:04.695Z'  // eslint-disable-line camelcase
   },
-  title_id: 1,  // eslint-disable-line camelcase
-  updated_at: '2020-12-21T15:23:15.866Z'  // eslint-disable-line camelcase
+  title_id: 2,  // eslint-disable-line camelcase
+  updated_at: '2021-01-14T15:23:15.866Z'  // eslint-disable-line camelcase
 };
 
 const loan: Loan = {
